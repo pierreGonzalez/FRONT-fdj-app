41d996a5ab60e5f54cc5f888e9469c328dc76890
pierreGonzalez <pierre.gonzalez@me.com>
2017-07-22 18:36:08 +0200

    maj Home

--- tp0/js/components/dumbComponents/home.js
+++ tp0/js/components/dumbComponents/home.js
@@ -9,9 +9,10 @@ export default class Home extends React.Component {
         <div className="jumbotron homeClass">
           <h1>Good day <code>lucky</code> boy !</h1>
           <blockquote className="blockquote">
-            <p>Tell me when you want to play, I will tell you the combinaton to choose ...</p>
-            <h6>but don&apos;t tell to anyone it&apos;s asecret ...</h6>
+            <p>Trust me I will tell you the combinaton to play*</p>
+            <h6>but don&apos;t tell to anyone it&apos;s a secret ...</h6>
             <footer>Combinaton generator v1.0.1</footer>
+            <footer>*This application is a personal code training</footer>
           </blockquote>
         </div>
       </div>
